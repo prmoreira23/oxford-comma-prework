95626461146ae07c3615118b29cd8be25cbbee48
Pablo Rocha Moreira <prm.gredes@gmail.com>
2018-04-16 04:08:52 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+    formatted_string = ""
+    if array.length == 1
+        formatted_string = array.first
+    elsif array.length == 2
+        formatted_string = array.join(" and ")
+    else
+        formatted_string = array[0..-2].join(", ") + ", and " + array[-1]
+    end
+    formatted_string
 end
